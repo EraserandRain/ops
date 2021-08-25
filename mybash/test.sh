1f4aa3084d4faf9e1b71dc39ef3c70b15afbd4b8
EraserandRain <1349291258@qq.com>
2021-08-25 18:13:35 +0800

    update links 0825

--- mybash/test.sh
+++ mybash/test.sh
@@ -8,7 +8,6 @@ docker-run-nginx='cd ~/wsl-testing/mydocker&&docker run --rm -d -p800:80 --name 
 dockergo='sudo service docker restart'
 dockerstop='sudo service docker stop'
 
-https://zhuanlan.zhihu.com/p/44874772
 https://zh.wikipedia.org/wiki/%E7%B6%B2%E8%B7%AF%E6%99%82%E9%96%93%E5%8D%94%E5%AE%9A
 https://www.jianshu.com/p/62028875d53e
 https://www.google.com/search?q=RFC1918
