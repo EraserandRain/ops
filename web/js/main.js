9c754d9596815d638d8019e834c1532789944f16
EraserandRain <1349291258@qq.com>
2021-12-02 18:36:57 +0800

    mistake

--- web/js/main.js
+++ web/js/main.js
@@ -1,9 +1,23 @@
-'use strict';
-function product(arr) {
-    var sum=arr.reduce(function (x, y) {
-        return x * y;
-    })
-    console.log(sum);
-}
-product([0,2,3]);
-module.exports = product;
+function f() {
+    try {
+      console.log(0);
+      throw 'bug';
+    } catch(e) {
+      console.log(1);
+      return true; // 这句原本会延迟到 finally 代码块结束再执行
+      console.log(2); // 不会运行
+    } finally {
+      console.log(3);
+      return false; // 这句会覆盖掉前面那句 return
+      console.log(4); // 不会运行
+    }
+  
+    console.log(5); // 不会运行
+  }
+  
+  var result = f();
+  // 0
+  // 1
+  // 3
+  
+  result
