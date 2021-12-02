78dd33e804774d86510c3e0644881bbffde5fa6b
EraserandRain <1349291258@qq.com>
2021-12-02 18:38:13 +0800

    mistake

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -3,7 +3,17 @@ export LC_ALL=C
 /etc/init.d/tias_defend restart
 /etc/init.d/tias_main restart
 /etc/init.d/tias_synd restart
+/etc/init.d/tias_sync restart
 /etc/init.d/tklogd restart
 /etc/init.d/tkmonitord restart
 /etc/init.d/tkserverd restart
+
+
+/etc/init.d/tias_defend stop
+/etc/init.d/tias_main stop
+/etc/init.d/tias_synd stop
+/etc/init.d/tias_sync stop
+/etc/init.d/tklogd stop
+/etc/init.d/tkmonitord stop
+/etc/init.d/tkserverd stop
 exit 0
