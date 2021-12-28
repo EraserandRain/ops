f34adc8629bb9e4a468ca3d5f431a6191b51d6b0
EraserandRain <1349291258@qq.com>
2021-12-28 23:56:36 +0800

    update scan.sh 2356

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -4,8 +4,8 @@ netconf=""
 iptxt=""
 read -p "input network config path(such as 'ifcfg-enp1s0'): " netconf
 read -p "input ip list path(txt file): " iptxt
-oldip=$(cat $netconf |grep IPADDR|awk -F '=' '{print $2}')
-sum=$(cat $iptxt|wc -l)
+oldip=$(cat "$netconf" |grep IPADDR|awk -F '=' '{print $2}')
+sum=$(cat "$iptxt"|wc -l)
 for (( i=0;i<"$sum";i++ ))
 do
     iparr[$i]=$(awk 'NR=="'$[$i+1]'"' $iptxt)
