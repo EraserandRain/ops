05b718b19949674df8eebae4b6e13ec5a4359a20
EraserandRain <1349291258@qq.com>
2021-12-31 18:43:39 +0800

    add webdev

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 export LC_ALL=C
-systemctl stop NetworkManager
-systemctl disable NetworkManager
 ## set input
-netconf=""
+eth=""
 txt=""
-read -p "Please input network config path(such as 'ifcfg-enp1s0'): " netconf
+read -p "Please input eth name (such as 'enp1s0'): " eth
 read -p "Please input ip list path(txt file): " txt
-oldip=$(cat "$netconf" |grep IPADDR|awk -F '=' '{print $2}')
 sum=$(cat "$txt"|wc -l)
 ## script loop
 for (( i=0;i<"$sum";i++ ))
 do
     iparr[$i]=$(awk 'NR=="'$[$i+1]'"' "$txt")
-    newip=${iparr[$i]}
-    echo $newip
-    sed -i "s/IPADDR=${oldip}/IPADDR=${newip}/" "$netconf"
-    oldip=$newip
-    service network restart && sh OS_Linux_check.sh "$newip"
+    ip=${iparr[$i]}
+    echo $ip
+    ip addr add "$ip" dev "$eth"
+    sh OS_Linux_check.sh "$ip"
 done
-systemctl enable NetworkManager
-systemctl start NetworkManager
+service network restart
 mv ./*.dat ./dat/
 echo success!
 exit 0
--- web/js/main.js
+++ web/js/main.js
@@ -1,23 +1,3 @@
-function f() {
-    try {
-      console.log(0);
-      throw 'bug';
-    } catch(e) {
-      console.log(1);
-      return true; // 这句原本会延迟到 finally 代码块结束再执行
-      console.log(2); // 不会运行
-    } finally {
-      console.log(3);
-      return false; // 这句会覆盖掉前面那句 return
-      console.log(4); // 不会运行
-    }
-  
-    console.log(5); // 不会运行
-  }
-  
-  var result = f();
-  // 0
-  // 1
-  // 3
-  
-  result
+var x = 3*4
+console.log(x)
+console.log("hello world");
