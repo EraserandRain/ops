ab283bc5fda7a1f4f0916f48d3918e91b8e553fe
EraserandRain <1349291258@qq.com>
2022-01-18 09:58:54 +0800

    add scripts/scan/iplist_018.txt

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,19 +1,13 @@
 #!bin/bash
 export LC_ALL=C
-/etc/init.d/tias_defend restart
-/etc/init.d/tias_main restart
-/etc/init.d/tias_synd restart
-/etc/init.d/tias_sync restart
-/etc/init.d/tklogd restart
-/etc/init.d/tkmonitord restart
-/etc/init.d/tkserverd restart
-
-
 /etc/init.d/tias_defend stop
+/etc/init.d/tias_logd stop
 /etc/init.d/tias_main stop
+/etc/init.d/tias_monitor stop
 /etc/init.d/tias_synd stop
-/etc/init.d/tias_sync stop
-/etc/init.d/tklogd stop
-/etc/init.d/tkmonitord stop
-/etc/init.d/tkserverd stop
-exit 0
+
+/etc/init.d/tias_defend restart
+/etc/init.d/tias_logd restart
+/etc/init.d/tias_main restart
+/etc/init.d/tias_monitor restart
+/etc/init.d/tias_synd restart
