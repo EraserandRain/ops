6be0d91b50ad534ec8083c0cd507e3907cd6ea51
EraserandRain <1349291258@qq.com>
2022-01-24 01:29:38 +0800

    \ create vue-demo move gohttpserver

---
+++ web-demo/vue-demo/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrapper">
+    <Cell v-on:click="clickCell" v-bind:n="n" />
+    <Cell v-on:click="clickCell" v-bind:n="n" />
+    <Cell v-on:click="clickCell" v-bind:n="n" />
+    <Cell v-on:click="clickCell" v-bind:n="n" />
+    <Cell v-on:click="clickCell" v-bind:n="n" />
+    <Cell v-on:click="clickCell" v-bind:n="n" />
+    <Cell v-on:click="clickCell" v-bind:n="n" />
+    <Cell v-on:click="clickCell" v-bind:n="n" />
+    <Cell v-on:click="clickCell" v-bind:n="n" />
+    <div>n 为 {{ n }}</div>
+  </div>
+</template>
+
+<script>
+import Cell from "./Cell";
+export default {
+  components: { Cell },
+  data() {
+    return { n: 0 };
+  },
+  methods: {
+    clickCell() {
+      console.log("halo");
+      this.n += 1;
+    },
+  },
+};
+</script>
+
+<style>
+.wrapper {
+  display: grid;
+  width: 300px;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+</style>
---
+++ web-demo/vue-demo/src/Cell.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div>{{ n }}</div>
+    <div class="cell" v-on:click="clickSelf">
+      <template v-if="a">{{ text }}</template>
+      <template v-else></template>
+    </div>
+  </div>
+</template>
+
+    <script>
+export default {
+  props: ["n"],
+  data: () => {
+    return { a: false, text: "" };
+  },
+  methods: {
+    clickSelf() {
+      if (this.text !== "") {
+        return;
+      }
+      this.a = true;
+      this.$emit("click");
+      this.text = this.n % 2 == 0 ? "x" : "o";
+    },
+  },
+};
+</script>
+
+<style>
+.cell {
+  color: red;
+  border: 1px solid #000;
+  height: 100px;
+  font-size: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
