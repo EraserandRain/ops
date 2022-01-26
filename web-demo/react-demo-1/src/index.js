e4fc3d03fbf582c98b650aa963d0c249f22b4ac2
EraserandRain <1349291258@qq.com>
2022-01-27 00:36:31 +0800

    add react-demo-1

---
+++ web-demo/react-demo-1/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const div = document.createElement('div')
+const p = document.createElement('p')
+const span = document.createElement('span')
+div.appendChild(p)
+p.appendChild(span)
+span.innerHTML="hello world"
+document.body.appendChild(div)
