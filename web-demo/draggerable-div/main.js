871c6d388c127d3b6719c47f00f0b973baee0865
EraserandRain <1349291258@qq.com>
2022-02-01 17:01:47 +0800

    update draggerable-div with ts

--- web-demo/draggerable-div/main.js
+++ web-demo/draggerable-div/main.js
@@ -1,25 +1,23 @@
 var div = document.createElement('div')
+div.id = "demo"
 document.body.appendChild(div)
 var dragging = false
-var lastX
-var lastY
+var position = [0, 0]
 div.onmousedown = function (e) {
     dragging = true
-    lastX = e.clientX
-    lastY = e.clientY
+    position = [e.clientX, e.clientY]
 }
 document.onmouseup = function () {
     dragging = false
 }
 document.onmousemove = function (e) {
     if (dragging === true) {
-        var deltaX = e.clientX - lastX
-        var deltaY = e.clientY - lastY
+        var deltaX = e.clientX - position[0]
+        var deltaY = e.clientY - position[1]
         var top = parseInt(div.style.top) || 0
         var left = parseInt(div.style.left) || 0
         div.style.top = top + deltaY + 'px'
         div.style.left = left + deltaX + 'px'
-        lastX = e.clientX
-        lastY = e.clientY
+        position = [e.clientX, e.clientY]
     }
 }
