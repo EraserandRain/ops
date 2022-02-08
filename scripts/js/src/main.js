f4a37c563f5f401447f0f90774cba666a9056432
EraserandRain <1349291258@qq.com>
2022-02-08 14:17:13 +0800

    fix: optimize  scripts/ contents

---
+++ scripts/js/src/main.js
@@ -0,0 +1,15 @@
+// var company = {
+//     address: 'beijing'
+// }
+// var yideng = Object.create(company);
+// delete yideng.address
+// console.log(yideng.address);
+
+
+// function test(){
+//     var btn = document.getElementById('btn')
+//     btn.onclick=function(){
+//         console.log(document.getElementById('name').value);
+//     }
+// }
+// test()
---
+++ scripts/vue/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="(item, index) in items" :key="item.message">
+        {{ item.message + " and " + index }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [{ message: "foo1" }, { message: "foo2" }],
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 10px;
+  margin: 20px;
+}
+.isFoo {
+  color: red;
+}
+</style>
