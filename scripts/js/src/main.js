596c08bb1704aa24da004fcefca816c657ab0d24
EraserandRain <1349291258@qq.com>
2022-02-09 18:32:46 +0800

    test: deploy babel env

--- scripts/js/src/main.js
+++ scripts/js/src/main.js
@@ -1,15 +1,1 @@
-// var company = {
-//     address: 'beijing'
-// }
-// var yideng = Object.create(company);
-// delete yideng.address
-// console.log(yideng.address);
-
-
-// function test(){
-//     var btn = document.getElementById('btn')
-//     btn.onclick=function(){
-//         console.log(document.getElementById('name').value);
-//     }
-// }
-// test()
+input.map(item => item + 1);
