9aa40a26a4946c18c45ad51da7b1452ad300c256
EraserandRain <1349291258@qq.com>
2022-02-11 11:12:44 +0800

    feat: add safe.sh  and react-project

---
+++ scripts/react-project/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const Test =()=>{
+  return (
+    <div>
+      <p>nihao</p>
+    </div>
+  )
+}
+ReactDOM.render(<Test />,document.getElementById('root'))
