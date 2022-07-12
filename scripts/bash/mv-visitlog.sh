65602df42ac654018ea48205e91463b273abf1cb
EraserandRain <1349291258@qq.com>
2022-07-12 17:36:59 +0800

    refactor: rename repo name and refactor project

--- scripts/bash/mv-visitlog.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export LC_ALL=C
-current_date=$(date -d '-1 day' '+%Y%m%d')
-cd /data
-mkdir -p "visitlog_${current_date}"
-find /visitlog/ -mtime +1 -regex '.*log$' -exec mv {} "/data/visitlog_${current_date}" \;
-echo "success"
-exit 0
